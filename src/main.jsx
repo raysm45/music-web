e4cb40f05b1b85a66957a804d8502de24bc3ab0c
raysm45 <sultansyamsuddin1@gmail.com>
2026-09-02 13:45:01 +0700

    Add am-lyrics import to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/global.css";
+import "./vendor/am-lyrics/am-lyrics.ts";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -14,4 +15,5 @@ if ("serviceWorker" in navigator) {
     navigator.serviceWorker.register("/sw.js").catch(() => {});
   });
 }
+
 
