205b7a30e1b7e54c38746e7e1ccb67ecd77e87ae
raysm45 <sultansyamsuddin1@gmail.com>
2026-09-03 13:35:28 +0700

    Add files via upload

--- src/pages/ServerDownPage.jsx
+++ src/pages/ServerDownPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
-const ServerDownPage = () => {
+export function ServerDownPage() {
   const [currentTime, setCurrentTime] = useState('');
 
   useEffect(() => {
@@ -339,6 +339,4 @@ const ServerDownPage = () => {
       </div>
     </>
   );
-};
-
-export default ServerDownPage;
+}
